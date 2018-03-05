61ef4d66573632027012ebd2c1adb037da976b64
humzamunir94 <humza.munir94@gmail.com>
2018-03-06 00:09:59 +0500

    toss page implemented(GUI)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 //import { TabsPage } from '../pages/tabs/tabs';
-import { AboutPage } from '../pages/about/about'
+import { AboutPage } from '../pages/about/about';
+import { GamePage } from '../pages/game/game'
+
 
 @Component({
   templateUrl: 'app.html'
@@ -24,7 +26,9 @@ export class MyApp {
     });
     this.pages = [
       { title: 'Home', component: HomePage }, 
-      { title: 'About Us', component: AboutPage }
+      { title: 'About Us', component: AboutPage },
+      { title: 'Game', component: GamePage }
+      
     ];
   }
   openPage(page){
